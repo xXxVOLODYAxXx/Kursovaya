1636e9b8e66e6ab3f7261f2040e356ad50e310e7
xXxVOLODYAxXx <sidorych200@gmail.com>
2021-11-22 00:07:54 +0300

    6

--- src/main/java/ru/sfedu/Kursovaya/utils/ArmyInfoTransformer.java
+++ src/main/java/ru/sfedu/Kursovaya/utils/ArmyInfoTransformer.java
@@ -10,22 +10,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ArmyInfoTransformer extends AbstractBeanField {
-    private String fieldsDelimiter="@";
-    private String elemDelimiter="#";
+    private String fieldsDelimiter = "@";
+    private String elemDelimiter = "#";
 
     @Override
     public Object convert(String value){
-        ArmyInfo armyInfo=new ArmyInfo();
+        ArmyInfo armyInfo = new ArmyInfo();
         String[] data = value.split(fieldsDelimiter);
         armyInfo.setArmyAttackPoints(Integer.parseInt(data[0]));
         armyInfo.setArmyHealthPoints(Integer.parseInt(data[1]));
-
         return armyInfo;
     }
 
     @Override
     public String convertToWrite(Object armyInfo){
-        ArmyInfo armyInfoList=(ArmyInfo) armyInfo;
+        ArmyInfo armyInfoList = (ArmyInfo) armyInfo;
         return String.format("%d"
                         +fieldsDelimiter
                         + "%d",
